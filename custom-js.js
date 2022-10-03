b869de2cea41a918f319063bd87e551416e3dcf4
Jil14 <114937315+Jil14@users.noreply.github.com>
2022-10-03 11:23:37 -0400

    Add files via upload

---
+++ custom-js.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded',(loaded) =>{
+
+  console.log('Your Flight  is  ready to go!!');
+
+//Using javascript for editing the body elements
+
+      document.getElementById("A1").style.background = "#85D118";
+      document.getElementById("A1").style.color = "#6E2C00";
+      document.getElementById("A1").style.textAlign = "center";
+
+        document.getElementById("A2").style.textAlign = "center";
+        document.getElementById("A3").style.textAlign = "center";
+
+        document.getElementById("A1").style.textAlign = "center";
+        document.getElementById("A1").style.textAlign = "center";
+
+
+      document.getElementById("B1").stylein.textAlign = "center";
+      document.getElementById("C1").stylecon.textAlign = "center";
+      document.getElementById("D1").style4.textAlign = "center";
+
+//Editing the header part of all page .
+
+    document.getElementById("indexhead").style.textAlign = "center";
+    document.getElementById("interiorhead").style.textAlign = "center";
+    document.getElementById("contacthead").stylecon.textAlign = "center";
+    document.getElementById("abouthead").style4.textAlign = "center";
+
+    document.getElementById("indexhead").style.background  = "#000";
+    document.getElementById("interiorhead").style.background = "#85D118";
+    document.getElementById("contacthead").stylecon.background = "#fbab60";
+    document.getElementById("abouthead").style.background = "#d2b48c";
+
+});
